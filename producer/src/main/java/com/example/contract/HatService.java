220b832810cf68287bd376334af3ae855223a798
Matt Raible <matt.raible@okta.com>
2022-02-01 10:30:07 -0700

    Tabs to spaces

--- producer/src/main/java/com/example/contract/HatService.java
+++ producer/src/main/java/com/example/contract/HatService.java
@@ -8,11 +8,13 @@ import java.util.Map;
 @Service
 public class HatService {
     private final Map<Long, Hat> hatMap;
+
     public HatService() {
         hatMap = new HashMap<>();
         hatMap.put(1L, new Hat(1L, "Sombrero", 30L, "red"));
         hatMap.put(2L, new Hat(2L, "Beanie", 5L, "blue"));
     }
+
     Hat findHatById(Long id) {
         return hatMap.get(id);
     }
